d6267d6125e7d0212fc995df84b7345e01338599
Edward Bustos <esbustos@indracompany.com>
2023-05-23 20:17:34 -0500

    routin fixis

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import { Box, Button, Container, Typography } from "@mui/material";
 
