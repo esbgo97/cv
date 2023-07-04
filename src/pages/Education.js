702d35253d5886abbf63671993a9a326299eed9f
Edward Bustos <esbgo97@gmail.com>
2023-07-04 15:02:29 -0500

    update pdf

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -5,9 +5,75 @@ import {
   AccordionSummary,
   Card,
   CardContent,
+  Box,
   Typography,
+  Grid,
+  Divider,
 } from "@mui/material";
 import React from "react";
+
+import macs_logo from "../assets/macs_logo.jpg";
+import sena_logo from "../assets/sena_logo.png";
+
+import plural_logo from "../assets/plural_logo.png";
+import udemy_logo from "../assets/udemy_logo.png";
+
+const studies = [
+  {
+    title: "ANALYST DEVELOPER",
+    institute: "Servicio Nacional de Aprendizaje (SENA)",
+    logo: sena_logo,
+    year: 2017,
+  },
+
+  {
+    title: "STATISTICS",
+    institute: "IED Marco Antonio Carreño Silva",
+    logo: macs_logo,
+    year: 2014,
+  },
+
+  {
+    title: "TECHNICAL DESIGN",
+    institute: "IED Marco Antonio Carreño Silva",
+    logo: macs_logo,
+    year: 2014,
+  },
+
+  {
+    title: "PHOTOGRAPHY",
+    institute: "IED Marco Antonio Carreño Silva",
+    logo: macs_logo,
+    year: 2014,
+  },
+  {
+    title: "HIGH SCHOOL",
+    institute: "IED Marco Antonio Carreño Silva",
+    logo: macs_logo,
+    year: 2014,
+  },
+];
+
+const courses = [
+  {
+    title: "JWT Authentication",
+    institute: "Udemy",
+    year: "2019",
+    logo: udemy_logo,
+  },
+  {
+    title: "SOA (Service Oriented Architecture)",
+    institute: "PluralSight",
+    year: "2017",
+    logo: plural_logo,
+  },
+  {
+    title: ".NET Developement",
+    institute: "PluralSight",
+    year: "2017",
+    logo: plural_logo,
+  },
+];
 
 const EducationItem = (props) => {
   const { info, index } = props;
@@ -18,7 +84,16 @@ const EducationItem = (props) => {
         aria-controls={`panel{index}a-content`}
         id={`panel{index}a-header`}
       >
-        <Typography color="primary.main">{info.title}</Typography>
+        <Grid container>
+          <Grid item md="9">
+            <Typography color="primary.main">{info.title} </Typography>
+            <Typography variant="caption">{info.institute}</Typography>
+          </Grid>
+
+          <Grid item md="3">
+            <img src={info.logo} height={50} />
+          </Grid>
+        </Grid>
       </AccordionSummary>
       <AccordionDetails>
         <Typography>
@@ -31,36 +106,7 @@ const EducationItem = (props) => {
     </Accordion>
   );
 };
-const studies = [
-  {
-    title: "ANALYST DEVELOPER",
-    institute: "Servicio Nacional de Aprendizaje (SENA)",
-    year: 2017,
-  },
 
-  {
-    title: "STATISTICS",
-    institute: "IED Marco Antonio Carreño Silva",
-    year: 2014,
-  },
-
-  {
-    title: "TECHNICAL DESIGN",
-    institute: "IED Marco Antonio Carreño Silva",
-    year: 2014,
-  },
-
-  {
-    title: "PHOTOGRAPHY",
-    institute: "IED Marco Antonio Carreño Silva",
-    year: 2014,
-  },
-  {
-    title: "HIGH SCHOOL",
-    institute: "IED Marco Antonio Carreño Silva",
-    year: 2014,
-  },
-];
 const Education = () => {
   return (
     <Card elevation={4}>
@@ -68,12 +114,36 @@ const Education = () => {
         <Typography variant="h4" color="primary.main">
           Education & Certificates
         </Typography>
-        {studies.map((info, i) => (
-          <>
-            <EducationItem kei={i} index={i} info={info} />
+        <br />
+        <Grid container spacing={2}>
+          <Grid item md sm={12} style={{ marginRight: 15 }}>
+            <Typography variant="h5" color="primary.main">
+              Institution Learning
+            </Typography>
             <br />
-          </>
-        ))}
+            {studies.map((info, i) => (
+              <>
+                <EducationItem kei={i} index={i} info={info} />
+                <br />
+              </>
+            ))}
+          </Grid>
+
+          <Divider orientation="vertical" flexItem />
+
+          <Grid item md sm={12}>
+            <Typography variant="h5" color="primary.main">
+              Self-Learning
+            </Typography>
+            <br />
+            {courses.map((info, i) => (
+              <>
+                <EducationItem kei={i} index={i} info={info} />
+                <br />
+              </>
+            ))}
+          </Grid>
+        </Grid>
       </CardContent>
     </Card>
   );
